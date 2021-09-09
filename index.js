ccd86c4283107d983ecdb2c791f134eafa2706e7
crazyCoder <apple.gh@mail.ru>
2021-09-09 16:08:18 +0300

    testing_database

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const { Telegraf, Murkup } = require("telegraf");
+const { Pool } = require("pg");
 require("dotenv").config();
 const text = require("./const");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+}).get("/db", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM test_table");
+    const results = { results: result ? result.row : null };
+    res.render("pages/db", results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send(err);
+  }
+});
 
 bot.start((ctx) => ctx.reply(`Hello ${ctx.message.from.first_name}`));
 bot.help((ctx) => ctx.reply(text.commands));
